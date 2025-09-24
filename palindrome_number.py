b408e5d6cce36713e9fceb819d7abd8c01bd5231
eliasakalu <eliasgetachew950@gmail.com>
2025-09-24 14:55:33 +0300

    Helps to check whether the number is palindrome or not

---
+++ palindrome_number.py
@@ -0,0 +1,14 @@
+def ispalindrome(n) :
+    rev = 0
+    temp = n
+    while temp != 0 :
+        ld = temp % 10
+        rev = rev * 10 + ld
+        temp = temp // 10
+    if (rev == n) :
+        print(True)
+    else :
+        print(False)
+if __name__== "__main__" :  
+    x = 789987
+    ispalindrome(x)
