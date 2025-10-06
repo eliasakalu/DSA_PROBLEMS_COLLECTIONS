afc65c6a8e8f302167d29a5e77eda1c46b1d8e91
eliasakalu <eliasgetachew950@gmail.com>
2025-10-06 14:55:08 +0300

    Add the problem of second largest number

---
+++ second_largest_element_in_a_list.py
@@ -0,0 +1,48 @@
+# 🚀 Efficient Approach
+# Time Complexity: O(n) | Auxiliary Space: O(1)
+
+def getSecMaxEfficient(l):
+    if len(l) <= 1:
+        return None
+    lar = l[0]
+    slar = None
+    for x in l:
+        if x > lar:
+            slar = lar
+            lar = x
+        elif x != lar:
+            if slar is None or x > slar:
+                slar = x
+    return slar
+
+# 🐢 Naive Approach
+# Time Complexity: O(n) | Auxiliary Space: O(1)
+
+def getMax(l):
+    if not l:
+        return None
+    res = l[0]
+    for i in range(len(l)):
+        if l[i] > res:
+            res = l[i]
+    return res
+
+def getSecMaxNaive(l):
+    if len(l) <= 1:
+        return None
+    lar = getMax(l)
+    slar = None
+    for i in l:
+        if i != lar:
+            if slar is None:
+                slar = i
+            else:
+                slar = max(i, slar)
+    return slar
+
+# 🧪 Driver Code
+if __name__ == "__main__":
+    List = [10, 5, 20, 8, 20]
+
+    print("🐢 Naive Second Max:", getSecMaxNaive(List))
+    print("⚡ Efficient Second Max:", getSecMaxEfficient(List))
