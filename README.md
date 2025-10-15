609c2f412542b6112ecea059cdc4fbbd3273a9c3
eliasakalu <eliasgetachew950@gmail.com>
2025-10-15 17:34:41 +0300

    Add Array Search problem

--- README.md
+++ README.md
@@ -41,3 +41,4 @@ A collection of Data Structures and Algorithms problems solved in Python.
 - [Peak Element](./peak_element.py) 
 - [Count 1s In The Binary Array](./count_1s_in_binary_array.py) 
 - [Floor in a Sorted Array](./floor_in_sorted_array.py) 
+- [Array Search](./array_search.py) 
---
+++ array_search.py
@@ -0,0 +1,24 @@
+'''
+Array Search
+Difficulty: BasicAccuracy: 40.95%Submissions: 492K+Points: 1Average Time: 15m
+Given an array, arr[] of n integers, and an integer element x, find whether element x is present in the array. Return the index of the first occurrence of x in the array, or -1 if it doesn't exist.
+
+Examples:
+
+Input: arr[] = [1, 2, 3, 4], x = 3
+Output: 2
+Explanation: For array [1, 2, 3, 4], the element to be searched is 3. Since 3 is present at index 2, the output is 2.
+Input: arr[] = [10, 8, 30, 4, 5], x = 5
+Output: 4
+Explanation: For array [10, 8, 30, 4, 5], the element to be searched is 5 and it is at index 4. So, the output is 4.
+Input: arr[] = [10, 8, 30], x = 6
+Output: -1
+Explanation: The element to be searched is 6 and it is not present, so we return -1.
+'''
+#Several times I did this so check my git repository 
+from binary_search import binarySearch # This is my binaryfunction inside binary_search.py
+
+arr= [1, 2, 3, 4]
+x = 3
+print(binarySearch(arr,x))
+
